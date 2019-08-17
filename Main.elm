2ae079cd0a443ee9b1544b9ff5f496ed1ed6e598
Rein van der Woerd <reinvanderwoerd@me.com>
2019-08-17 14:24:54 +0200

    Example data

--- Main.elm
+++ Main.elm
@@ -23,6 +23,32 @@ type Entity
 type alias Edge = Int
 
 
+
+sampleData : Graph
+sampleData = 
+  let
+    nodes =
+      [ Graph.Node 0 
+        { name = "Marsha Blackburn"
+        , entity = Politician
+        , position = Point2d.origin
+        }
+      , Graph.Node 1 
+        { name = "Marsha Blackburn"
+        , entity = PAC
+        , position = Point2d.origin
+        }
+      ]
+
+    e from to =
+      Graph.Edge from to 
+
+    edges =
+      [ e 0 1 1000
+      ]
+  in
+    Graph.fromNodesAndEdges nodes edges
+
 main =
   text "Hello, world!"
 
