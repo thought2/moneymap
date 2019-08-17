bd064875e0485e413b1aedc5485c297714c63ab9
Rein van der Woerd <reinvanderwoerd@me.com>
2019-08-17 14:16:50 +0200

    data structure

--- Main.elm
+++ Main.elm
@@ -1,5 +1,28 @@
 import Html exposing (text)
+import Graph
+import Point2d exposing (Point2d)
+
+type alias Graph = Graph.Graph Node Edge
+
+type Party 
+  = Democrat
+  | Republican
+
+type alias Node =
+  { name : String
+  , entity : Entity
+  , position : Point2d
+  }
+
+type Entity 
+  = Politician
+  | Company
+  | PAC
+  | Individual
+
+type alias Edge = Int
 
 
 main =
   text "Hello, world!"
+
