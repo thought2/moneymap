75b2789be4c433eefa41241b23ca506b6e322a15
Shandaram <anohinaksenia1701@gmail.com>
2019-08-17 14:39:11 +0200

    Update Main.elm
    candidates added

--- Main.elm
+++ Main.elm
@@ -15,12 +15,12 @@ type alias Node =
   }
 
 type Entity 
-  = Politician
+  = Politician {party : Party}
   | Company
   | PAC
   | Individual
 
-type alias Edge = Int
+type alias Edge = { money : Int }
 
 
 
@@ -29,22 +29,56 @@ sampleData =
   let
     nodes =
       [ Graph.Node 0 
-        { name = "Marsha Blackburn"
-        , entity = Politician
+        { name = "NRA"
+        , entity = Company
         , position = Point2d.origin
         }
       , Graph.Node 1 
         { name = "Marsha Blackburn"
-        , entity = PAC
+        , entity = Politician {party = Republican}
+        , position = Point2d.origin
+        }
+        , Graph.Node 2 
+        { name = "Ted Cruz"
+        , entity = Politician {party = Republican}
+        , position = Point2d.origin
+        }
+         , Graph.Node 3 
+        { name = "John Culberson"
+        , entity = Politician {party = Republican}
+        , position = Point2d.origin
+        }
+          , Graph.Node 4 
+        { name = "John Faso"
+        , entity = Politician {party = Republican}
+        , position = Point2d.origin
+        }
+          , Graph.Node 5 
+        { name = "Josh Hawley"
+        , entity = Politician {party = Republican}
+        , position = Point2d.origin
+        }
+          , Graph.Node 6 
+        { name = "Collin Peterson"
+        , entity = Politician {party = Democrat}
+        , position = Point2d.origin
+        }
+         , Graph.Node 7 
+        { name = "Henry Cuellar"
+        , entity = Politician {party = Democrat}
         , position = Point2d.origin
         }
       ]
 
-    e from to =
-      Graph.Edge from to 
-
     edges =
-      [ e 0 1 1000
+      [ Graph.Edge 0 1 { money = 15800}
+      Graph.Edge 0 2 { money = 9900}
+      Graph.Edge 0 3 { money = 9900}
+      Graph.Edge 0 4 { money = 9900}
+      Graph.Edge 0 5 { money = 9900}
+      Graph.Edge 0 6 { money = 9900}
+      Graph.Edge 0 7 { money = 6950}
+
       ]
   in
     Graph.fromNodesAndEdges nodes edges
